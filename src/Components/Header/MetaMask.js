e68a88fd3e345da7a584433da40ac2349bdfdeab
sventaylor <96111859+sventaylor@users.noreply.github.com>
2022-06-19 16:50:00 +0200

    button functionality added

--- src/Components/Header/MetaMask.js
+++ src/Components/Header/MetaMask.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { ethers } from "ethers";
 
 const MetaMask = () => {
-  return (
-    <div>
-        <button>MetaMask</button>
-    </div>
-  )
-}
+  const [walletAddress, setWalletAddress] = useState("");
 
-export default MetaMask
+  async function requestAccount() {
+    console.log("Requesting account...");
+
+    if (window.ethereum) {
+      console.log("detected");
+
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log("Error connecting...");
+      }
+    } else {
+      alert("Meta Mask not detected");
+    }
+  }
+
+  async function connectWallet() {
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+    }
+  }
+
+  return (
+    <div className="walletButton">
+      <header className="walletConnect">
+        <button onClick={requestAccount}>Connect Wallet</button>
+        <h3>Wallet Address: {walletAddress}</h3>
+      </header>
+    </div>
+  );
+};
+
+export default MetaMask;
