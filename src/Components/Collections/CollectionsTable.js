a9db88b46f006f7596753830cb22c597cdcf0d87
Nina Schmidt <nina.schmidt.be@gmail.com>
2022-06-24 20:48:43 +0200

    added collection fetching

--- src/Components/Collections/CollectionsTable.js
+++ src/Components/Collections/CollectionsTable.js
@@ -5,17 +5,34 @@ import SalesFloor from './SalesFloor'
 import TopLabel from '../Wallets/TopLabel'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import fetchData, { trendingCollections } from '../Utils/CollectionFetch'
+ 
 
 const CollectionsTable = () => {
 
   const [dataCollection, setDataCollection] = useState([]);
 
   useEffect(() => {
+    (async () => {
+      const { queryResult, error } = await fetchData(trendingCollections);
+      if (error) {
+        return console.error(error)
+      }
+      console.log('IT IS HAPPENING HERE QUERYRESULT', queryResult)
+    })()
+  }, [])
+
+  //const [dataCollection, setDataCollection] = useState([]);
+
+
+
+  /*useEffect(() => {
     axios('https://jsonplaceholder.typicode.com/users') 
       .then(res => setDataCollection(res.data.slice(0, 10)))
       .catch(err => console.log(err))
   }, []);
-
+*/
+console.log('THIS IS THE DATA COLLECTION', dataCollection)
   return (
     <div className='collectionsTable'>
       <div className='tableHeader'>
@@ -28,6 +45,7 @@ const CollectionsTable = () => {
         <div className='singleCollectionSales'>Sales</div>
       </div> 
       {dataCollection.length && dataCollection.map(collectionItem => {
+        console.log('HERE IS THE DATA', collectionItem)
         return (
           <div className="singleCollectionContainer" key={collectionItem.id}>
             <SingleCollection data={collectionItem} />
---
+++ src/Components/Utils/CollectionFetch.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+export const trendingCollections = ` {
+    contracts(orderBy: SALES, orderDirection: DESC) {
+        edges {
+          node {
+            address
+            ... on ERC721Contract {
+              name
+              stats {
+                totalSales
+                average
+                ceiling
+                floor
+                volume
+              }
+              symbol
+            }
+            tokens {
+              edges {
+                node {
+                  tokenId
+                  images {
+                    height
+                    mimeType
+                    url
+                    width
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`
+
+
+const fetchData = async (query) => {
+    try {
+        const queryResult = await axios.post(process.env.REACT_APP_GRAPHQL_API, {
+            query
+        }, {
+            headers: {
+                "x-api-key": process.env.REACT_APP_X_API_KEY
+            }
+        });
+        return { queryResult }
+    } catch (error) {
+        return { error }
+    }
+};
+
+export default fetchData
