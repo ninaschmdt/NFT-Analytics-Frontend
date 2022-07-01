8402200b15a847a7924ec8d161a5f299e164d59b
Nina Schmidt <nina.schmidt.be@gmail.com>
2022-07-01 14:55:04 +0200

    add icy router

--- src/Components/Wallets/SingleWallet.js
+++ src/Components/Wallets/SingleWallet.js
@@ -2,23 +2,12 @@ import React from "react";
 
 const SingleWallet = ({ wallet }) => {
   return (
-<<<<<<< HEAD
-    <div className='singleWallet'>
-      <div className='singleWalletImageNameAddress'>
-        <div className='walletImage'>
-        </div>
-        <div className='singleWalletNameAddress'>
-          <div className='walletName'>
-            {/* {walletName} */}
-          </div>
-=======
     <div className="singleWallet">
       <div className="singleWalletImageNameAddress">
         <div className="walletImage"></div>
         <div className="singleWalletNameAddress">
           <div className="walletName">{wallet.tokenName}</div>
           <div className="walletAddress">{/* {address} */}</div>
->>>>>>> sven3
         </div>
       </div>
       <div>
