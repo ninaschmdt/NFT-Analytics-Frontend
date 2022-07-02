4c2bbd8a5013b930b296d726ae107b4f64a769c0
theactofshining <ruslan.e.valikhanov@gmail.com>
2022-07-02 16:42:11 +0200

    working css

--- src/Components/Collections/SalesFloor.js
+++ src/Components/Collections/SalesFloor.js
@@ -2,6 +2,7 @@ import React from 'react'
 import SalesFloorChange from './SalesFloorChange'
 
 const SalesFloor = ( {data} ) => {
+  const salesFloorChange = 
   return (
     <div className='sales'>{data}
     <SalesFloorChange />
@@ -10,3 +11,9 @@ const SalesFloor = ( {data} ) => {
 }
 
 export default SalesFloor
+
+//take a value from salesfloor
+//store it into local storage
+//watch this value
+//when value changes -> make math
+//
--- src/Components/Wallets/AddWallet.js
+++ src/Components/Wallets/AddWallet.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AddWallet = () => {
   return (
     <div className='addWallet'>
-      <h3>Track Wallet</h3>
+      <h1>Track Wallet</h1>
       <form>
         <input type="text" id="fname" name="fname" placeholder="Paste wallet ID here..." /><br />
         <input type="submit" className='button' value="Add" />
