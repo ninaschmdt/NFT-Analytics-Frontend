bab11f600d8ccdb58b55ed41d2a8ae078868e14f
theactofshining <ruslan.e.valikhanov@gmail.com>
2022-07-04 11:56:55 +0200

    Co-authored-by: Jorge Paul <weyvern@users.noreply.github.com>

--- src/Components/Collections/CollectionImage.js
+++ src/Components/Collections/CollectionImage.js
@@ -1,26 +1,25 @@
 import React from 'react'
 const CollectionImage = ({ node }) => {
 
+  const fallBack = 'https://static.toiimg.com/thumb/msid-67586673,width-800,height-600,resizemode-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg'
+
+
   const onError = (e) => {
-    return <img className='collectionImage'>e.currentTarget.src = 'https://static.toiimg.com/thumb/msid-67586673,width-800,height-600,resizemode-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg'</img> 
+    return e.currentTarget.src = fallBack
   }
 
   const renderImage = () => {
-    if (node.tokens.edges.length) {
-      if (node.tokens.edges[0].node.images.length) {
-        return <img className='collectionImage' src={node.tokens.edges[0].node.images[0].url} onError={onError} />
-      } else {
-        return <img className='collectionImage' src='https://static.toiimg.com/thumb/msid-67586673,width-800,height-600,resizemode-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg' width='56px' onError={onError} />
-      }
+    if (node.token?.images.length   ) {
+      return <img className='collectionImage' src={node.token.images[0].url} onError={onError} />
     }
     else {
-      return <img className='collectionImage' src='https://static.toiimg.com/thumb/msid-67586673,width-800,height-600,resizemode-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg' width='56px' onError={onError} />
+      return <img className='collectionImage' src={fallBack} width='56px' onError={onError} />
     }
   }
 
   return (
     <div>
-      {renderImage()}
+    {renderImage()}
     </div>
   )
 }
